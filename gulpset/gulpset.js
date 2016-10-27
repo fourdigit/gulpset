ac82ed50658d6c0b80427afda9bbb47c728d7837
Nobuyuki Nishigaki <nyki.ngki@gmail.com>
2016-10-27 22:23:42 +0900

    fixed license related stuff

--- gulpset/gulpset.js
+++ gulpset/gulpset.js
@@ -1,3 +1,14 @@
+/**
+ * @project gulpset
+ * @author Nobuyuki Nishigaki <nishigaki@4digit.jp>
+ * @license
+ * Copyright FOURDIGIT DESIGN Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/fourdigitdesign/gulpset/blob/master/LICENSE
+ */
+
+
 // imports
 var gulp = require("gulp-task-doc");
 var glob = require("glob");
