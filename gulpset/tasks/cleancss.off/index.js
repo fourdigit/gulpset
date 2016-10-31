296af20167c0081edbee78eafd354afc9ab11cfb
Nobuyuki Nishigaki <nyki.ngki@gmail.com>
2016-10-31 13:36:00 +0900

    added ejs & cleancss tasks

---
+++ gulpset/tasks/cleancss.off/index.js
@@ -0,0 +1,44 @@
+var gulpset = require("./../../gulpset");
+
+
+// @verbose
+gulpset.gulp.task("cleancss",			function() { return gulpset.tasks.cleancss(false); });
+// @verbose
+gulpset.gulp.task("cleancss-minify",	function() { return gulpset.tasks.cleancss(true); });
+
+
+gulpset.confs.cleancss = {
+	src: [gulpset.paths.src + "**/*.css"],
+	dest: gulpset.paths.dest
+};
+
+
+
+//----------------------------------------------------------------------------------------------------
+///
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var gulpif = require("gulp-if");
+var cleancss = require("gulp-clean-css");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulpset.tasks.cleancss = function(doMinify, browsers, conf) {
+	if(doMinify === undefined) doMinify = false;
+	conf = conf || gulpset.confs.cleancss || {};
+	conf.browsers = conf.browsers || ["last 3 versions"];
+	if(browsers) conf.browsers = browsers;
+
+	var options = {};
+	if(doMinify) options.outputStyle = "compressed";
+
+	return gulp.src(conf.src)
+		.pipe(plumber())
+		.pipe(gulpif(doMinify !== true, sourcemaps.init()))
+		.pipe(cleancss())
+		.pipe(postcss([autoprefixer({browsers: conf.browsers})]))
+		.pipe(gulpif(doMinify !== true, sourcemaps.write("./")))
+		.pipe(gulp.dest(conf.dest))
+		.pipe(gulpset.stream({match: "**/*.css"}));
+};
---
+++ gulpset/tasks/ejs.off/index.js
@@ -0,0 +1,31 @@
+var gulpset = require("./../../gulpset");
+
+
+// @verbose
+gulpset.gulp.task("ejs",	function() { return gulpset.tasks.ejs(false); });
+
+
+gulpset.confs.ejs = {
+	src: [gulpset.paths.src + "**/*.html"],
+	dest: gulpset.paths.dest,
+	data: {}
+};
+
+
+
+//----------------------------------------------------------------------------------------------------
+///
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var ejs = require("gulp-ejs");
+
+gulpset.tasks.ejs = function(data, conf) {
+	data = data || gulpset.confs.ejs.data || {};
+	conf = conf || gulpset.confs.ejs || {};
+
+	return gulp.src(conf.src)
+		.pipe(plumber())
+		.pipe(ejs(data))
+		.pipe(gulp.dest(conf.dest))
+		.pipe(gulpset.stream());
+};
