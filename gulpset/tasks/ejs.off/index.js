0687b9b441b46ffc4ee65645f3953f1eab77927b
rieimaoka <ri_ie@me.com>
2017-10-05 11:06:10 +0900

    ejsのtaskを修正

--- gulpset/tasks/ejs.off/index.js
+++ gulpset/tasks/ejs.off/index.js
@@ -6,8 +6,15 @@ gulpset.gulp.task("ejs",	function() { return gulpset.tasks.ejs(); });
 
 
 gulpset.confs.ejs = {
-	src: [gulpset.paths.src + "**/*.ejs"],
-	dest: gulpset.paths.dest
+	src: [gulpset.paths.src + "**/*.html"],
+	dest: gulpset.paths.dest,
+	data: {},
+	options: {
+		root: process.cwd()
+	},
+	settings: {
+		ext: ".html"
+	}
 };
 
 
@@ -19,19 +26,16 @@ var plumber = require("gulp-plumber");
 var ejs = require("gulp-ejs");
 var changed = require("gulp-changed");
 
-gulpset.tasks.ejs = function(data, conf) {
+gulpset.tasks.ejs = function(data, options, settings, conf) {
 	data = data || gulpset.confs.ejs.data || {};
+	options = options || gulpset.confs.ejs.options || {};
+	settings = settings || gulpset.confs.ejs.settings || {};
 	conf = conf || gulpset.confs.ejs || {};
 
 	return gulp.src(conf.src)
 		.pipe(plumber())
 		.pipe(changed(conf.dest))
-		.pipe(ejs({}, {
-			root: process.cwd() + gulpset.paths.src,
-		}, {
-				ext: '.html'
-			}
-		))
+		.pipe(ejs(data, options, settings))
 		.pipe(gulp.dest(conf.dest))
 		.pipe(gulpset.stream());
 };
