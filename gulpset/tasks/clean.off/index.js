ee60d47459f78bf02fe731d5e78a90e47a130b75
rieimaoka <ri_ie@me.com>
2018-07-04 17:42:42 +0900

    タイポあったので修正しました => task -> tasks

--- gulpset/tasks/clean.off/index.js
+++ gulpset/tasks/clean.off/index.js
@@ -1,14 +1,14 @@
 const gulpset = require("./../../gulpset");
 
 // @verbose
-gulpset.gulp.task("clean", () => gulpset.task.clean());
+gulpset.gulp.task("clean", () => gulpset.tasks.clean());
 
 //----------------------------------------------------------------------------------------------------
 //
 
 const del = require("del");
 
-gulpset.task.clean = path => {
+gulpset.tasks.clean = path => {
   path = path || gulpset.paths.dest;
   return del(path);
 };
