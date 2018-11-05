226dd89befd904f3f2560d529cae38370fac0cb3
nghiant <tronghiant@users.noreply.github.com>
2018-11-05 16:25:26 +0700

    feat: remove sass from docz
    `sass` task already handle that stuff

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,2 @@
 module.exports = {
-  modifyBundlerConfig: config => {
-    config.resolve.extensions.push('.scss');
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    });
-
-    return config;
-  }
 };
