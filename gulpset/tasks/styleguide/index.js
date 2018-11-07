d4674bfcfee9644323682f07e0247c86c81ab579
nghiant <tronghiant@users.noreply.github.com>
2018-11-07 17:59:47 +0700

    fix(styleguide task): travis-ci errror
    `No output has been received in the last 10m0s`

--- gulpset/tasks/styleguide/index.js
+++ gulpset/tasks/styleguide/index.js
@@ -2,13 +2,13 @@ const gulpset = require('./../../gulpset');
 
 // // @verbose
 gulpset.gulp.task('styleguide', () => gulpset.tasks.styleguide('dev'));
-gulpset.gulp.task('styleguide:prod', () => gulpset.tasks.styleguide('prod'));
+gulpset.gulp.task('styleguide:prod', (cb) => gulpset.tasks.styleguide('prod', cb));
 
 const { commands, args: defaultArgs } = require('docz-core');
 const execDevCommand = async args => commands.dev(args);
 const execBuildCommand = async args => commands.build(args);
 
-gulpset.tasks.styleguide = mod => {
+gulpset.tasks.styleguide = (mod, cb) => {
   const Config = {};
   const fakeYargs = {
     positional: (key, opt) => {
@@ -17,6 +17,16 @@ gulpset.tasks.styleguide = mod => {
   };
   // https://github.com/pedronauck/docz/blob/be94b0e/packages/docz-core/src/commands/args.ts#L96
   defaultArgs(mod === 'dev' ? 'development' : 'production')(fakeYargs);
-  return mod === 'dev' ? execDevCommand(Config) : execBuildCommand(Config);
-  // return execCommand(mod === 'dev' ? 'dev' : 'build')(Config);
+
+  if (mod === 'dev' ) return execDevCommand(Config);
+
+  return execBuildCommand(Config).then(() => {
+    if (cb) cb();
+    process.exit(0);
+  }, (reason) => {
+    console.error('`styleguide` build FAILED!');
+    console.error(reason);
+    if (cb) cb();
+    process.exit(1);
+  });
 };
