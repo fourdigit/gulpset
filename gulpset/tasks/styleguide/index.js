39b62a81f65cae39f911a0081a237e48f9c66ecb
nghiant <tronghiant@users.noreply.github.com>
2018-11-13 11:21:20 +0700

    refactor(styleguide task): lint✨

--- gulpset/tasks/styleguide/index.js
+++ gulpset/tasks/styleguide/index.js
@@ -2,7 +2,7 @@ const gulpset = require('./../../gulpset');
 
 // // @verbose
 gulpset.gulp.task('styleguide', () => gulpset.tasks.styleguide('dev'));
-gulpset.gulp.task('styleguide:prod', (cb) => gulpset.tasks.styleguide('prod', cb));
+gulpset.gulp.task('styleguide:prod', cb => gulpset.tasks.styleguide('prod', cb));
 
 const { commands, args: defaultArgs } = require('docz-core');
 const execDevCommand = async args => commands.dev(args);
@@ -18,15 +18,20 @@ gulpset.tasks.styleguide = (mod, cb) => {
   // https://github.com/pedronauck/docz/blob/be94b0e/packages/docz-core/src/commands/args.ts#L96
   defaultArgs(mod === 'dev' ? 'development' : 'production')(fakeYargs);
 
-  if (mod === 'dev' ) return execDevCommand(Config);
+  if (mod === 'dev') return execDevCommand(Config);
 
-  return execBuildCommand(Config).then(() => {
-    if (cb) cb();
-    process.exit(0);
-  }, (reason) => {
-    console.error('`styleguide` build FAILED!');
-    console.error(reason);
-    if (cb) cb();
-    process.exit(1);
-  });
+  return execBuildCommand(Config).then(
+    () => {
+      if (cb) cb();
+      process.exit(0);
+    },
+    reason => {
+      // eslint-disable-next-line no-console
+      console.error('`styleguide` build FAILED!');
+      // eslint-disable-next-line no-console
+      console.error(reason);
+      if (cb) cb();
+      process.exit(1);
+    }
+  );
 };
